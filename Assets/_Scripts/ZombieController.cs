9c14c4b4ccfb78c6b6d535551515a9fb3b7d45de
JotaPe <63083839+iJotape4@users.noreply.github.com>
2022-05-02 00:24:06 -0500

    Animated Zombie Prefab
    -Hay que agregar colliders a las animaciones

--- Assets/_Scripts/ZombieController.cs
+++ Assets/_Scripts/ZombieController.cs
@@ -3,20 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent (typeof (Rigidbody))]
-[RequireComponent (typeof (Collider))]
-[RequireComponent (typeof (Animator))]
 
 public class ZombieController : MonoBehaviour
 {
-    private Rigidbody _rb;
     private Animator _anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
-        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
