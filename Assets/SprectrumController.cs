8f0f1152c539cf5d4d752d5acce7120a25f726d2
JotaPe <jcamacho249@unab.edu.co>
2022-05-19 21:21:05 -0500

    Spectrum Launch A BlueFire on Start

--- Assets/SprectrumController.cs
+++ Assets/SprectrumController.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class SprectrumController : MonoBehaviour
 {
     public Transform _playerTarget;
-    public ParticleSystem _blueFire;
-    public ParticleSystem _xplosion;
+    public Transform _spawnPoint;
+    public GameObject _blueFire;
 
-
+    [SerializeField] private float shotForce = 10f;
     // Start is called before the first frame update
     void Start()
     {
         _playerTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        _blueFire = GetComponentInChildren<ParticleSystem>();
-        _xplosion = _blueFire.gameObject.GetComponentInChildren<ParticleSystem>();
+        _spawnPoint = transform.GetChild(0);
+        _blueFire = Resources.Load<GameObject>("Prefabs/BlueFire");
+        LaunchFireBall();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void LaunchFireBall()
+    {
+        transform.LookAt(_playerTarget);
+        GameObject FireBall;
+        FireBall = Instantiate(_blueFire, _spawnPoint.transform);
+        FireBall.transform.parent = null;
+        FireBall.GetComponent<Rigidbody>().AddForce(_spawnPoint.forward * shotForce);
+        FireBall.transform.LookAt(_playerTarget);      
     }
 }
