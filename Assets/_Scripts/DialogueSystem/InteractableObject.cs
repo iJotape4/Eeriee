48ed60fc97891909f444f95d099b292ef21eadc2
JotaPe <jcamacho249@unab.edu.co>
2022-05-20 22:21:13 -0500

    Input System Final Version

--- Assets/_Scripts/DialogueSystem/InteractableObject.cs
+++ Assets/_Scripts/DialogueSystem/InteractableObject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))] 
 public class InteractableObject : MonoBehaviour
 {
     public bool _finishedEvent;
