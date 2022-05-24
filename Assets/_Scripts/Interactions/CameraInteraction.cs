2a5aafb20aa11c210456dfcb5337ac0a32ab9b34
JotaPe <jcamacho249@unab.edu.co>
2022-05-24 00:36:39 -0500

    Update CameraInteraction.cs
    Ahora solo se puede recoger teniendo las manos vacias

--- Assets/_Scripts/Interactions/CameraInteraction.cs
+++ Assets/_Scripts/Interactions/CameraInteraction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using StarterAssets;
 
 public class CameraInteraction : MonoBehaviour
 {
@@ -9,11 +10,14 @@ public class CameraInteraction : MonoBehaviour
     public float _rayDistance =3f;
     private PlayerInput _playerInput;
     private InputAction _interact;
+    private FirstPersonController _player;
     void Start()
     {
         _camera = this.transform;
-        _playerInput = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>();
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>();
+        _playerInput = _player.GetComponent<PlayerInput>();
         _interact = _playerInput.actions["Interact"];
+        
     }
 
     // Update is called once per frame
@@ -21,7 +25,7 @@ public class CameraInteraction : MonoBehaviour
     {
         Debug.DrawRay(_camera.position, _camera.forward * _rayDistance, Color.red); 
 
-        if (_interact.WasPressedThisFrame()){
+        if (_interact.WasPressedThisFrame() && _player._currentWeaponIndex==0){
             RaycastHit hit;
             if (Physics.Raycast(_camera.position, _camera.forward, out hit, _rayDistance, LayerMask.GetMask("Interactable"))){
                 hit.transform.GetComponent<Interactable>().Interact();
