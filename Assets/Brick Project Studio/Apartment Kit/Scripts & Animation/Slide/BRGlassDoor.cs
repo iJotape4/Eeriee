17d3be4a17a55c49427874575ffb4d0c7a0cd323
JotaPe <jcamacho249@unab.edu.co>
2022-05-25 17:37:34 -0500

    Appartment all interactions finished

--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Slide/BRGlassDoor.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Slide/BRGlassDoor.cs
@@ -5,68 +5,20 @@ using UnityEngine;
 namespace SojaExiles
 
 {
-	public class BRGlassDoor : MonoBehaviour
-	{
+    public class BRGlassDoor : Interactable
+    {
+        public Animator _openAndClose;
+        public string opened = "Opened";
 
-		public Animator openandclose;
-		public bool open;
-		public Transform Player;
+        public void Start()
+        {
+            _openAndClose = GetComponent<Animator>();
+        }
 
-		void Start()
-		{
-			open = false;
-		}
-
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
-
-						}
-
-					}
-				}
-
-			}
-
-		}
-
-		IEnumerator opening()
-		{
-			print("you are opening");
-			openandclose.Play("BRGlassDoorOpen");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing");
-			openandclose.Play("BRGlassDoorClose");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
-
-
-	}
+        public override void Interact()
+        {
+            base.Interact();
+            _openAndClose.SetBool(opened, _openAndClose.GetBool(opened) ? false : true);
+        }
+    }
 }
--- Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Z Axis -1/Drawer_Pull_Zopp.cs
+++ Assets/Brick Project Studio/Apartment Kit/Scripts & Animation/Z Axis -1/Drawer_Pull_Zopp.cs
@@ -5,69 +5,20 @@ using UnityEngine;
 namespace SojaExiles
 
 {
-	public class Drawer_Pull_Zopp : MonoBehaviour
-	{
+    public class Drawer_Pull_Zopp : Interactable
+    {
+        public Animator _openAndClose;
+        public string opened = "Opened";
 
-		public Animator pull;
-		public bool open;
-		public Transform Player;
+        public void Start()
+        {
+            _openAndClose = GetComponent<Animator>();
+        }
 
-		void Start()
-		{
-			open = false;
-		}
-
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 10)
-					{
-						print("object name");
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
-
-						}
-
-					}
-				}
-
-			}
-
-		}
-
-		IEnumerator opening()
-		{
-			print("you are opening the door");
-			pull.Play("openpullopp");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing the door");
-			pull.Play("closepushopp");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
-
-
-	}
+        public override void Interact()
+        {
+            base.Interact();
+            _openAndClose.SetBool(opened, _openAndClose.GetBool(opened) ? false : true);
+        }
+    }
 }
