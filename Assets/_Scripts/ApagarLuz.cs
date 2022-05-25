1f5ba11a7da6bb50d3b43ff2a0acd5d073bf8f75
JotaPe <jcamacho249@unab.edu.co>
2022-05-25 17:56:02 -0500

    Tiempo actualizado a 66

--- Assets/_Scripts/ApagarLuz.cs
+++ Assets/_Scripts/ApagarLuz.cs
@@ -7,7 +7,7 @@ public class ApagarLuz : MonoBehaviour
 {
     public GameObject luces;
     public PlayerInput _playerInput;
-    private float tiempo = 6f;
+    private float tiempo = 66f;
     public InteractableObject _eventConversation;
     // Start is called before the first frame update
     void Start()
